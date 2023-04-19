95aa16abd6020ae1eb2b64b264341c8319c86190
GenaBylkin <anton.nemanixin@mail.ru>
2023-04-19 18:40:09 +0300

    add new packages

--- app/src/main/java/com/example/testaplication/presenstation/MainActivity.kt
+++ app/src/main/java/com/example/testaplication/presenstation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.testaplication
+package com.example.testaplication.presenstation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testaplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
