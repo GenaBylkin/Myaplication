42f7bd298bd1536e01553c7dacb6ce2d3b28ab3d
GenaBylkin <anton.nemanixin@mail.ru>
2023-04-20 19:32:16 +0300

    add impl

--- app/src/main/java/com/example/testaplication/domain/ShopList.kt
+++ app/src/main/java/com/example/testaplication/domain/ShopList.kt
@@ -3,6 +3,10 @@ package com.example.testaplication.domain
 data class ShopList(
     val name: String,
     val quantity: Int,
-    val id: Int,
-    val active: Boolean
-)
+    val active: Boolean,
+    var id: Int = UNDEFINED_ID
+){
+    companion object{
+        const val UNDEFINED_ID = -1
+    }
+}
