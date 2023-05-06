9fa5642e997cbe4fdb2ee17ad5631742286170ca
GenaBylkin <anton.nemanixin@mail.ru>
2023-05-06 16:29:17 +0300

    completion shop item fragment

--- app/src/main/java/com/example/testaplication/presenstation/ShopItemFragment.kt
+++ app/src/main/java/com/example/testaplication/presenstation/ShopItemFragment.kt
@@ -1,13 +1,33 @@
 package com.example.testaplication.presenstation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.testaplication.R
+import com.example.testaplication.domain.ShopList
+import com.google.android.material.textfield.TextInputLayout
 
-class ShopItemFragment: Fragment() {
+class ShopItemFragment(
+    private val screenMode: String = SCREEN_MODE_UNKNOWN,
+    private val shopItemId: Int = ShopList.UNDEFINED_ID
+): Fragment() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var etName: EditText
+    private lateinit var etCount: EditText
+    private lateinit var button: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,5 +36,135 @@ class ShopItemFragment: Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_shop_item, container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        parseIntent()
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        initViews(view)
+        editTextListeners()
+        launchMode()
+        observeViewModel()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+        private var SCREEN_MODE_UNKNOWN = ""
+
+
+        fun newIntentItemAdd(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun editIntentItem(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
+            return intent
+        }
+
+    }
+
+    private fun initViews(view: View) {
+        tilName = view.findViewById(R.id.til_name)
+        tilCount = view.findViewById(R.id.til_count)
+        etName = view.findViewById(R.id.et_name)
+        etCount = view.findViewById(R.id.et_count)
+        button = view.findViewById(R.id.save_button)
+    }
+
+    private fun parseIntent() {
+        if (screenMode != MODE_EDIT && screenMode != MODE_ADD) {
+            throw RuntimeException("Param screen mode is absent")
+        }
+
+        if (screenMode == MODE_EDIT && shopItemId == ShopList.UNDEFINED_ID) {
+            throw RuntimeException("Param shop item id unknown")
+        }
+    }
+
+    private fun editItem(){
+        viewModel.getShopList(shopItemId)
+        viewModel.itemShopList.observe(viewLifecycleOwner) {
+            etName.setText(it.name)
+            etCount.setText(it.quantity.toString())
+
+        }
+        button.setOnClickListener {
+            viewModel.editShopItem(etName.text?.toString(),etCount.text?.toString())
+        }
+    }
+
+    private fun addItem() {
+        button.setOnClickListener {
+            viewModel.addNewShopItem(etName.text?.toString(),etCount.text?.toString())
+        }
+    }
+
+    private fun editTextListeners() {
+        etName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetInputName()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+        etCount.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetInputCount()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    private fun launchMode(){
+        when (screenMode) {
+            MODE_EDIT -> editItem()
+            MODE_ADD -> addItem()
+            else -> throw RuntimeException("Unknown this screen mode: $screenMode")
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.errorInputCount.observe(viewLifecycleOwner){
+            val message =  if (it) {
+                getString(R.string.error_input_count)
+            } else {
+                null
+            }
+            tilCount.error = message
+        }
+        viewModel.errorInputName.observe(viewLifecycleOwner){
+            val message =  if (it) {
+                getString(R.string.error_input_name)
+            } else {
+                null
+            }
+            tilName.error = message
+        }
+
+        viewModel.closeScreen.observe(viewLifecycleOwner){
+            //finish()
+        }
     }
 }
